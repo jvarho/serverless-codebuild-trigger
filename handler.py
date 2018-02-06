9842e8f0ab5d29ecb9d1ce9f05ce2bffc7891617
Jan Varho <jan@varho.org>
2018-02-06 17:42:39 +0200

    Fix branch environment variable

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ import os
 
 REGION=os.environ['region']
 PROJECT=os.environ['project']
-BRANCH=os.environ.get('project')
+BRANCH=os.environ.get('branch')
 
 
 codebuild = boto3.client('codebuild', region_name=REGION)
